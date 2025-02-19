06b17c79d75750ffda379ee3cdcf255841966f5f
chenghuan zhong <>
2025-02-19 18:49:10 +0800

    1231231

--- layout_study/lib/main.dart
+++ layout_study/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:layout_study/%E5%A4%9A%E4%B8%AAcontainer%E6%B5%8B%E8%AF%95.dart';
 import 'package:layout_study/%E6%B5%8B%E8%AF%95initState%E8%B0%83%E7%94%A8%E6%97%B6%E6%9C%BA.dart';
+import 'package:layout_study/%E8%87%AA%E5%AE%9A%E4%B9%89Viewport%E6%B5%8B%E8%AF%95.dart';
 import 'package:layout_study/%E8%BE%B9%E7%95%8C%E5%B8%83%E5%B1%80%E6%B5%8B%E8%AF%95.dart';
 import 'package:layout_study/Stack%E6%B5%8B%E8%AF%95.dart';
 import 'package:layout_study/sizedByParent%E5%8F%98%E9%87%8F%E6%B5%8B%E8%AF%95.dart';
@@ -29,7 +30,8 @@ void main() {
   // runApp(RowTest.makeWidget());
   // runApp(DuoGeContainerTest.makeWidget());
   // runApp(MyStackTest.makeWidget());
-  runApp(CustomScrollViewExample());
+  // runApp(CustomScrollViewExample());
+  runApp(MyViewport());
   // runApp(MyCustomMultiChildLayoutTest.makeWidget());
   // runApp(MaterialApp(home: MyRelayoutBoundary()));
 
--- layout_study/lib/自定义Viewport测试.dart
+++ layout_study/lib/自定义Viewport测试.dart
@@ -1,73 +1,50 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter/rendering.dart';
-//
-// /// 自定义滚动视口组件
-// class CustomScrollViewDemo extends SingleChildRenderObjectWidget {
-//   const CustomScrollViewDemo({super.key});
-//
-//   @override
-//   RenderObject createRenderObject(BuildContext context) {
-//     // 创建 ScrollController 管理滚动位置
-//     final ScrollController controller = ScrollController();
-//
-//     return RenderViewport(
-//       axisDirection: AxisDirection.down, // 垂直滚动
-//       offset: controller.position, // 绑定滚动控制器
-//       cacheExtent: 200.0, // 预渲染200像素区域
-//       children: [
-//         // 列表内容
-//         _buildSliverList(),
-//       ], crossAxisDirection: AxisDirection.right,
-//     );
-//   }
-// }
-//
-//
-//
-// /// 视差头部代理类
-// class _ParallaxHeaderDelegate extends SliverPersistentHeaderDelegate {
-//   @override
-//   Widget build(
-//       BuildContext context, double shrinkOffset, bool overlapsContent) {
-//     return Container(
-//       height: 200,
-//       color: Colors.blue,
-//       alignment: Alignment.center,
-//       child: Transform.translate(
-//         offset: Offset(0, -shrinkOffset * 0.5), // 视差效果
-//         child: const Text(
-//           'Parallax Header',
-//           style: TextStyle(color: Colors.white, fontSize: 24),
-//         ),
-//       ),
-//     );
-//   }
-//
-//   @override
-//   double get maxExtent => 200; // 最大高度
-//
-//   @override
-//   double get minExtent => 100; // 最小高度
-//
-//   @override
-//   bool shouldRebuild(_ParallaxHeaderDelegate oldDelegate) => true;
-// }
-//
-// /// 构建列表 Sliver
-// RenderSliverList _buildSliverList() {
-//   return RenderSliverList(
-//     childManager: SliverChildListDelegate(
-//       List.generate(
-//         // 生成50个列表项
-//         50,
-//         (index) => Container(
-//           height: 80,
-//           color: index.isEven ? Colors.grey[200] : Colors.white,
-//           alignment: Alignment.center,
-//           child: Text('Item $index'),
-//         ),
-//       ),
-//     ),
-//   );
-// }
-//
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class MyViewport extends StatelessWidget {
+  MyViewport({Key? key}) : super(key: key);
+
+  final List<int> data = List.generate(60, (index) => index + 1);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Scrollable+Viewport 测试'),
+      ),
+      body: Scrollable(
+        viewportBuilder: _buildViewPort,
+      ),
+    );
+  }
+
+  Widget _buildViewPort(BuildContext context, ViewportOffset position) {
+    return Viewport(
+      offset: position,
+      slivers: [_buildSliverGrid(), _buildSliverList()],
+    );
+  }
+
+  Widget _buildSliverList() {
+    return SliverList(
+        delegate: SliverChildBuilderDelegate(
+      _buildItemByIndex,
+      childCount: data.length,
+    ));
+  }
+
+  Widget _buildItemByIndex(BuildContext context, int index) {
+    return Text('${data[index]}');
+  }
+
+  Widget _buildSliverGrid() {
+    return SliverGrid(
+        delegate: SliverChildBuilderDelegate(
+          _buildItemByIndex,
+          childCount: data.length,
+        ),
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 10));
+  }
+}
